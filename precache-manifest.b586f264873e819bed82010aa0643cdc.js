e734633e68669e4d3d4e05db8130d3b69ed28f9f
Cindy Burton <cindy.brton@gmail.com>
2020-05-18 17:44:33 -0600

    Updates

---
+++ precache-manifest.b586f264873e819bed82010aa0643cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e20ecf0f092bbf7060298fb8712e980",
+    "url": "/portfoli0/index.html"
+  },
+  {
+    "revision": "52253a890d43efe8d0ed",
+    "url": "/portfoli0/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/portfoli0/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfoli0/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52253a890d43efe8d0ed",
+    "url": "/portfoli0/static/js/main.e825aa83.chunk.js"
+  },
+  {
+    "revision": "ea117b585c9cb5a24718",
+    "url": "/portfoli0/static/js/runtime-main.7deb7403.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfoli0/static/media/logo.5d5d9eef.svg"
+  }
+]);
