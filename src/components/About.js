ba94a23894bcb4201a6b30ea11604f6a611c86cd
Cindy Burton <cindy.brton@gmail.com>
2020-05-18 18:13:49 -0600

    stuff

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>YO BITCH THIS IS A PAGE ABOUT ME</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>YO BITCH THIS IS A PAGE ABOUT ME</h1>
+        </div>
+    )
+}
+
+export default Contact;
