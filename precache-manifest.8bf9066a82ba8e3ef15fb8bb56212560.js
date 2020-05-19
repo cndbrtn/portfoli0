c7ba836bd6fac988795155aaafc4ad45c2fe69d5
Cindy Burton <cindy.brton@gmail.com>
2020-05-18 21:54:39 -0600

    Updates

---
+++ precache-manifest.8bf9066a82ba8e3ef15fb8bb56212560.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00c3758fa67a099d20a",
+    "url": "/portfoli0/static/js/main.62c3ca22.chunk.js"
+  },
+  {
+    "revision": "719f911ab54b31e22561",
+    "url": "/portfoli0/static/js/runtime~main.bedce127.js"
+  },
+  {
+    "revision": "c3ab7b27161d3828f4e4",
+    "url": "/portfoli0/static/js/2.4f40f86d.chunk.js"
+  },
+  {
+    "revision": "475e4b529cbdcb3d574a3c669115bcb5",
+    "url": "/portfoli0/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfoli0/precache-manifest.b586f264873e819bed82010aa0643cdc.js"
+  "/portfoli0/precache-manifest.8bf9066a82ba8e3ef15fb8bb56212560.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfoli0/index.html"), {
+workbox.routing.registerNavigationRoute("/portfoli0/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
